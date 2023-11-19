7adfd7ef6aba28d739a3b30cc4c9875f34c0f7a0
Yasin <146708314+cheeseburgerhere@users.noreply.github.com>
2023-11-19 22:59:42 +0300

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,24 @@ sys.path.append(parent_directory)
 
 import unittest
 
+#test cases for webscraper
+
+from modules import utils
+
+class TestClass(unittest.TestCase):
+    def test_null(self):
+        with self.assertRaises(TypeError):
+            utils.scrape_news(time_range="10 days ago",selected_topics="",num_articles=2)
+    def test_invalidNumber(self):    
+        with self.assertRaises(IndexError):
+            utils.scrape_news(time_range="10 days ago",selected_topics="Technology news",num_articles=-1)
+    def test_type(self):
+        with self.assertRaises(TypeError):
+            utils.scrape_news(time_range="10 days ago",selected_topics=7,num_articles=2)
+    def test_date(self):
+        with self.assertRaises(TypeError):
+            utils.scrape_news(time_range="why",selected_topics="Technology news",num_articles=2)
+#end test cases for webscraper
 
 class TestNewsCraft(unittest.TestCase):
     pass
