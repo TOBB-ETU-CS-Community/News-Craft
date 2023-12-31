cba02d15b0393b5d127aca04c24ae70abff85dc2
Yasin <146708314+cheeseburgerhere@users.noreply.github.com>
2023-12-31 13:27:14 +0300

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -26,6 +26,32 @@ class TestClass(unittest.TestCase):
             scrape_news(time_range="why",selected_topics="Technology news",num_articles=2)
 #end test cases for webscraper
 
+#TEST CASES FOR SUMMARY FUNCTION
+from modules.utils import generate_summaries,ReadingTime,ComplexityLevel
+import pandas as pd
+
+class TestClass(unittest.TestCase):
+    def test_null(self):
+        with self.assertRaises(ValueError):
+            df=pd.DataFrame()
+            generate_summaries(df,ComplexityLevel.EASY,ReadingTime.ONE_MINUTE)
+    def test_invalidNumber(self):    
+        with self.assertRaises(ValueError):
+            df=pd.DataFrame({
+                "Content":[],
+                "Test":["test","test"]
+            })
+            generate_summaries(df,ComplexityLevel.EASY,ReadingTime.ONE_MINUTE)
+    def test_type(self):
+        with self.assertRaises(ValueError):
+            df=scrape_news("10 days ago","world",2)
+            temp:pd.DataFrame=df.iloc[:,0:-1]
+            outputToSeries=pd.Series([])
+            outputToSeries.name="Summaries"
+            df=pd.merge(temp,outputToSeries,right_index=True,left_index=True)
+            generate_summaries(df,ComplexityLevel.EASY,ReadingTime.ONE_MINUTE)
+
+#END
 class TestNewsCraft(unittest.TestCase):
     pass
 
